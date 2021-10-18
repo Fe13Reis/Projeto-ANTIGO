8794c732e048c0c5440573562f25635a7413f4f8
Fe13Reis <54784592+Fe13Reis@users.noreply.github.com>
2021-10-18 00:06:25 -0300

    Update aplicativo.py
    Código em desenvolvimento, python / flask para postar as páginas do projeto.

--- python_flask/aplicativo.py
+++ python_flask/aplicativo.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template, redirect, request
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'mestra' == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+app.run(debug=True)
